70bb952738b9077f5d060c19dd77803c91599f11
deepoversampling <deepoversampling@gmail.com>
2024-10-27 18:01:40 +0100

    building application attempt 8

--- src/test/java/com/jettjavuar/springdemo/SpringdemoApplicationTests.java
+++ src/test/java/com/jettjavuar/springdemo/SpringdemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.jettjavuar.springdemo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class SpringdemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
