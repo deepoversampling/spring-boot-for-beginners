cda1a555b2e6151f5b94ec5d0740059d0954e3fb
deepoversampling <deepoversampling@gmail.com>
2024-10-28 13:59:46 +0100

    f

--- src/test/java/com/jettjavuar/springdemo/SpringdemoApplicationTests.java
+++ src/test/java/com/jettjavuar/springdemo/SpringdemoApplicationTests.java
@@ -1,14 +1,10 @@
 package com.jettjavuar.springdemo;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringdemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
